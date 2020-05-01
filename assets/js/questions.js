77404907c5e0164d2986bddb972fca1f4d1f603c
Gael Gohoungo <gohoungogael@gmail.com>
2020-04-30 23:47:29 -0400

    added questions to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -14,7 +14,7 @@ const quizQuestions = [
     {
         question: "What is the capital of England?",
         choices: ["Manchester", "Liverpool", "London", "Birmingham"],
-        correctAnswer: "London"
+        correctAnswer: "Londonn"
     },
 
     {
